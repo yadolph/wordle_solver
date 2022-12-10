49f63acd47b30df25e8c3716a12cdfeb22741f59
Ivan <oiv@live.ru>
2022-12-10 17:06:12 +0500

    v.1.1
    Код упрощен.
    Исправлен баг, когда в некоторых случаях буква из исключений по позиции не исключала слова.

--- main.py
+++ main.py
@@ -1,76 +1,66 @@
 import re
 
 
-def game_solver(lines_lst_filtered):
+def create_word_lst(filename):
+    lines_lst = []
+    with open(filename, encoding='UTF-8') as f:
+        lines = f.read()
+        lines_lst = lines.split()
+    return [x for x in lines_lst if len(x) == 5]
+
+
+def game_solver(all_words):
     excl = list(input('Исключения без пробелов:'))
     incl = list(input('Буквы в наличии без пробелов:'))
-    mask = list(input('Маска слова с * вместо неизвестных:'))
+    corr_mask = list(input('Маска слова с * вместо неизвестных:'))
     wrong_mask = list(input('Маска исключений:'))
 
     lst_to_excl = []
 
-    for word in lines_lst_filtered:
-        for letter in excl:
-            if letter in word:
-                lst_to_excl.append(word)
+    for letter in excl:
+        lst_to_excl.extend([x for x in all_words if letter in x])
+    lst_to_excl = list(set(lst_to_excl))
 
-    lst_after_excl = []
-    for word in lines_lst_filtered:
-        if not word in lst_to_excl:
-            lst_after_excl.append(word)
+    lst_after_excl = [x for x in all_words if not x in lst_to_excl]
 
     lst_after_incl = []
     for word in lst_after_excl:
-        counter = 0
-        for letter in incl:
-            if letter in word:
-                counter += 1
-        if counter == len(incl):
+        if all([char in word for char in incl]):
             lst_after_incl.append(word)
 
     word_enumerates = {x:0 for x in lst_after_incl}
     words_candidates = []
-    mask_letters = [x for x in mask if x != '*']
+    corr_mask_letters = [x for x in corr_mask if x != '*']
 
-    for position, symbol in enumerate(mask):
-        if mask == ['*','*','*','*','*']:
+    for position, symbol in enumerate(corr_mask):
+        if not corr_mask_letters:
             words_candidates = lst_after_incl
             break
         if symbol == '*':
             continue
         
         for word in lst_after_incl:
-            if re.search(symbol, word[position], re.IGNORECASE):
+            if re.search(symbol, word[position], re.I):
                 word_enumerates[word] += 1
         
         for word, freq in word_enumerates.items():
-            if freq == len(mask_letters):
+            if freq == len(corr_mask_letters):
                 words_candidates.append(word)
     
-    print(wrong_mask)
-
     for position, symbol in enumerate(wrong_mask):
         if wrong_mask == ['*','*','*','*','*']:
             break
         if symbol == '*':
             continue
-        for word in words_candidates:
-            if re.search(symbol, word[position], re.IGNORECASE):
-                words_candidates.remove(word)
+        words_candidates = [
+            x for x in words_candidates if not re.search(symbol, x[position], re.I)
+        ]
 
-    print(words_candidates)
+    return(words_candidates)
 
 
 if __name__ == '__main__':
-    lines_lst = []
-
-    with open('russian_nouns.txt', encoding='UTF-8') as f:
-        lines = f.read()
-        lines_lst = lines.split()
-
-    lines_lst_filtered = [x for x in lines_lst if len(x) == 5]
-
+    all_words = create_word_lst('russian_nouns.txt')
     print('Введите слово "ОСИНА"')
-
     while True:
-        game_solver(lines_lst_filtered)
+        print(game_solver(all_words))
